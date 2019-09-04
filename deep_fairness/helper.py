4c966fa6f021c864bb370108ad888f79c5fd95cc
anumoshsad <anumoshsad@gmail.com>
2019-09-04 19:52:27 -0400

    helper function added

--- deep_fairness/helper.py
+++ deep_fairness/helper.py
@@ -10,6 +10,9 @@ import time
 import glob
 import torch.nn as nn
 
+import pandas as pd
+
+
 def ab_ret(x,y):# x=a-b, y=a/b
     b=x/(y-1)
     a= b*y
@@ -123,6 +126,52 @@ def pickle_load(file_path):
         return pickle.load(MacOSFile(f))
 
 
+
+def convert_dict_to_categorical(df):
+    # convert dataframe from one hot encoded to categorical for gender rance
+    df_for_metric = pd.DataFrame({})
+    df_for_metric['view'] = df['view']
+    
+    protected_df = pd.DataFrame(data = df['a'], index=None, 
+                                columns = ['male', 'female', 'gender_other', 'white', 'black_or_african_american',
+                                                               'asian','race_other'] )
+    
+    gender_df = protected_df[['male','female','gender_other']]
+    race_df = protected_df[['white', 'black_or_african_american','asian','race_other']]
+    
+
+    gender_race = pd.DataFrame({})
+    gender_race['gender'] = gender_df.idxmax(1).tolist()
+    gender_race['race'] =  race_df.idxmax(1).tolist()
+    
+    df_for_metric['gender'] = gender_race['gender']
+    df_for_metric['race'] = gender_race['race']
+
+    
+    rating_df = pd.DataFrame(data = df['rating'], index=None, columns = ['beautiful', 'confusing',
+                              'courageous', 'fascinating', 'funny', 'informative', 'ingenious',
+                              'inspiring', 'jaw-dropping', 'longwinded', 'obnoxious', 'ok',
+                              'persuasive', 'unconvincing'] )
+    
+    df_for_metric[['beautiful', 'confusing',
+                              'courageous', 'fascinating', 'funny', 'informative', 'ingenious',
+                              'inspiring', 'jaw-dropping', 'longwinded', 'obnoxious', 'ok',
+                              'persuasive', 'unconvincing']] = rating_df[['beautiful', 'confusing',
+                              'courageous', 'fascinating', 'funny', 'informative', 'ingenious',
+                              'inspiring', 'jaw-dropping', 'longwinded', 'obnoxious', 'ok',
+                              'persuasive', 'unconvincing']]
+
+
+    # for transcripts use 50 columns for doc2vec50
+    array = df['transcript']
+    temp_transcript = pd.DataFrame(data=array[:,:],    # values
+                                   index=None,    # 1st column as index
+                                   columns=['t_'+str(i) for i in range(1,array.shape[1]+1)])
+
+    for col in temp_transcript.columns:
+        df_for_metric[col] = temp_transcript[col]
+        
+    return df_for_metric
 
 
 
